db621d102c65ff690ad19425a0e0d23a58f62e1e
VICTOR EVANGELISTA CANDIDO DOS SANTOS <victor.evangelista@alunoescolassociais.org.br>
2024-10-24 12:28:07 -0300

    .

--- converter.js
+++ converter.js
@@ -1,11 +1,18 @@
 document.getElementById('currencyForm').addEventListener('submit', function(event){
     event.preventDefault(); //Previne a atualização automática do navegador.
-    const valor = parseFloat(document.getElementById('amount').value); // Cria uma variável do valor inserido pelo usuário(que tal pode ser número quebrado Ex. 1,23).
-
 
     //Função para obter os valores inseridos pelo usuário no select
+    
+    const valor = parseFloat(document.getElementById('amount').value); // Cria uma variável do valor inserido pelo usuário(que tal pode ser número quebrado Ex. 1,23).
     const daMoeda = document.getElementById('daMoeda').value;
     const paraMoeda = document.getElementById('paraMoeda').value;
 
+
+    let valorConvertido;
+    if(daMoeda === paraMoeda){
+        valorConvertido = valor;
+    }else{
+
+    }
 
 })
